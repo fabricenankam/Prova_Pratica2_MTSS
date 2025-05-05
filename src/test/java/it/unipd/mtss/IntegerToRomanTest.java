b32d7b9d1e1c69ab102eb2ebd9475f5ea18c89e2
Yiguzhu <yiguzhu2@gmail.com>
2025-05-05 19:17:01 +0200

    close #1 inizializzato git flow, maven e creati main e test iniziali

---
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,38 @@
+package it.unipd.mtss;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class IntegerToRomanTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public IntegerToRomanTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( IntegerToRomanTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
