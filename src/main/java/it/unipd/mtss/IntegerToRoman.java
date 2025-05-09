7146fedbf915e55fe4d920ec3498f411cbd304d8
Yiguzhu <yiguzhu2@gmail.com>
2025-05-09 17:02:07 +0200

    fixing errors

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,20 +6,20 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-	public static String convert(int number){
-		if(number > 0 && number <= 1000){
-			String Roman = "";
-			while(number > 0){
-				//Issue #4
-				if(number % 5 > 0){
-					number--;
-					Roman = Roman + "I";
-				}
-			}
-			return Roman;
-		}
-		else{
-			throw new IllegalArgumentException("Input deve essere un numero tra 1 e 1000.");
-		}
-	}
+  public static String convert(int number){
+    if(number > 0 && number <= 1000){
+      String Roman = "";
+      while(number > 0){
+        //Issue #4
+        if(number % 5 > 0){
+          number--;
+          Roman = Roman + "I";
+        }
+      }
+      return Roman;
+    }
+    else{
+      throw new IllegalArgumentException("Input deve essere un numero tra 1 e 1000.");
+    }
+  }
 }
